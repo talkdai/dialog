2a978d716efbcd851b4744bf4633f74ba81bc814
Vinicius Mesel <4984147+vmesel@users.noreply.github.com>
2023-11-15 11:45:59 -0300

    Fixes database-url

--- app/models/db.py
+++ app/models/db.py
@@ -3,12 +3,12 @@ from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker
 
 try:
-    from app.settings import DB_URL
+    from app.settings import DATABASE_URL
 except:
-    from settings import DB_URL
+    from settings import DATABASE_URL
 
 
-engine = create_engine(DB_URL)
+engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 session = Session()
 session.execute(text('CREATE EXTENSION IF NOT EXISTS vector'))
