db52ddb85c520661c5683e8a05051f4284db9fc7
Vinicius Mesel <4984147+vmesel@users.noreply.github.com>
2023-11-15 12:52:43 -0300

    Adds data loading at beginning

--- app/load_csv.py
+++ app/load_csv.py
@@ -16,6 +16,17 @@ def load_csv_and_generate_embeddings(path):
             raise Exception(f"Column {col} not found in {path}")
 
     df = df[necessary_cols]
+
+    df_in_db = pd.read_sql(
+        text(f"SELECT question, content FROM {CompanyContent.__tablename__}"),
+        session.get_bind()
+    )
+
+    df = df[~df["question"].isin(df_in_db["question"])]
+
+    print("Generating embeddings for new questions...")
+    print("New questions:", len(df))
+
     df["embedding"] = generate_embeddings(df["content"])
     df.to_sql(
         CompanyContent.__tablename__,
@@ -26,7 +37,7 @@ def load_csv_and_generate_embeddings(path):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--path", type=str, required=True)
+    parser.add_argument("--path", type=str, required=False, default="/data/data.csv")
     args = parser.parse_args()
 
     load_csv_and_generate_embeddings(args.path)
