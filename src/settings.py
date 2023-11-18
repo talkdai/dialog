01808b3df2f1ea2ca93fa794a9a8a15842938d28
Avelino <hey@avelino.run>
2023-11-18 07:21:22 -0300

    project config empty, if not exist file
    Signed-off-by: Avelino <hey@avelino.run>

--- src/settings.py
+++ src/settings.py
@@ -6,5 +6,6 @@ DATABASE_URL = config("DATABASE_URL")
 OPENAI_API_KEY = config("OPENAI_API_KEY")
 VERBOSE_LLM = config("VERBOSE_LLM", default=False, cast=bool)
 
+PROJECT_CONFIG = {}
 with open(config("PROJECT_CONFIG"), "rb") as f:
     PROJECT_CONFIG = tomllib.load(f)
