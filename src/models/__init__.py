889497b318523dd1b5a351f3d2bc4faf5c1c5110
Avelino <hey@avelino.run>
2023-11-19 09:53:32 -0300

    create chat message model
    Signed-off-by: Avelino <hey@avelino.run>

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from pgvector.sqlalchemy import Vector
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.dialects.postgresql import JSONB, TIMESTAMP
 from sqlalchemy.orm import declarative_base
 
 from .db import engine
@@ -12,6 +15,16 @@ class Chat(Base):
 
     uuid = Column(String, primary_key=True)
     tags = Column(String, nullable=True)
+
+
+class ChatMessages(Base):
+    __tablename__ = "chat_messages"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    parent = Column(Integer, nullable=True)
+    session_id = Column(String, nullable=False)
+    message = Column(JSONB, nullable=False)
+    timestamp = Column(TIMESTAMP, nullable=False, default=datetime.utcnow)
 
 
 class CompanyContent(Base):
