9d8fa5345a04aca7ac0c0598f949e08fcf468b45
Walison Filipe <walisonfilipe@hotmail.com>
2024-02-08 11:59:52 -0300

    Fix run.sh conditional sintax

--- etc/run.sh
+++ etc/run.sh
@@ -3,12 +3,12 @@
 alembic upgrade head
 python load_csv.py --path ${DIALOG_DATA_PATH}
 
-if "${TEST}"; then
+if  [ -n "${TEST}" ]; then
     python -m unittest
     exit 0
 fi
 
-if "${DEBUG}"; then
+if [ -n "${DEBUG}" ]; then
     uvicorn main:app --host 0.0.0.0 --port ${PORT} --reload
 else
     uvicorn main:app --host 0.0.0.0 --port ${PORT}
