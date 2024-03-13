e1f013ff9c3bce0b1191c477e387f92a8494ef11
Vinicius Mesel <4984147+vmesel@users.noreply.github.com>
2024-03-13 15:29:36 -0300

    Partial: Migrating tests to pytest

--- src/dialog/settings.py
+++ src/dialog/settings.py
@@ -1,17 +1,19 @@
 import tomllib
 from pathlib import Path
 
-from decouple import Csv, config
+from decouple import Csv, Config
 
-LOGGING_LEVEL = config("LOGGING_LEVEL", default="INFO")
+config = Config(".env")
 
-DATABASE_URL = config("DATABASE_URL")
-OPENAI_API_KEY = config("OPENAI_API_KEY")
-VERBOSE_LLM = config("VERBOSE_LLM", default=False, cast=bool)
-PLUGINS = config("PLUGINS", cast=Csv(), default=None)
+LOGGING_LEVEL = config.get("LOGGING_LEVEL", default="INFO")
+
+DATABASE_URL = config.get("DATABASE_URL")
+OPENAI_API_KEY = config.get("OPENAI_API_KEY")
+VERBOSE_LLM = config.get("VERBOSE_LLM", default=False, cast=bool)
+PLUGINS = config.get("PLUGINS", cast=Csv(), default=None)
 
 # Project (`.toml`) configuration
-PROJECT_CONFIG = config(
+PROJECT_CONFIG = config.get(
     "PROJECT_CONFIG",
     cast=lambda filename: tomllib.loads(Path(filename).read_text()),
     default={},
@@ -21,15 +23,15 @@ FALLBACK = PROJECT_PROMPT.get("fallback")
 FALLBACK_NOT_FOUND_RELEVANT_CONTENTS = PROJECT_PROMPT.get("fallback_not_found_relevant_contents")
 
 # Langchain and LLM parameters and settings
-LLM_CLASS = config("LLM_CLASS", default=None)
-LLM_TEMPERATURE = config("LLM_TEMPERATURE", default=0.2, cast=float)
-LLM_RELEVANT_CONTENTS = config("LLM_RELEVANT_CONTENTS", default=1, cast=int)
-LLM_MEMORY_SIZE = config("LLM_MEMORY_SIZE", default=5, cast=int)
-STATIC_FILE_LOCATION = config("STATIC_FILE_LOCATION", "/app/static")
-COSINE_SIMILARITY_THRESHOLD = config("CONSINE_SIMILARITY_THRESHOLD", default=0.2, cast=float)
+LLM_CLASS = config.get("LLM_CLASS", default=None)
+LLM_TEMPERATURE = config.get("LLM_TEMPERATURE", default=0.2, cast=float)
+LLM_RELEVANT_CONTENTS = config.get("LLM_RELEVANT_CONTENTS", default=1, cast=int)
+LLM_MEMORY_SIZE = config.get("LLM_MEMORY_SIZE", default=5, cast=int)
+STATIC_FILE_LOCATION = config.get("STATIC_FILE_LOCATION", "/app/static")
+COSINE_SIMILARITY_THRESHOLD = config.get("COSINE_SIMILARITY_THRESHOLD", default=0.2, cast=float)
 
 # Cors
-CORS_ALLOW_ORIGINS = config("CORS_ALLOW_ORIGINS", cast=Csv(), default="*")
-CORS_ALLOW_CREDENTIALS = config("CORS_ALLOW_CREDENTIALS", cast=bool, default=True)
-CORS_ALLOW_METHODS = config("CORS_ALLOW_METHODS", cast=Csv(), default="*")
-CORS_ALLOW_HEADERS = config("CORS_ALLOW_HEADERS", cast=Csv(), default="*")
+CORS_ALLOW_ORIGINS = config.get("CORS_ALLOW_ORIGINS", cast=Csv(), default="*")
+CORS_ALLOW_CREDENTIALS = config.get("CORS_ALLOW_CREDENTIALS", cast=bool, default=True)
+CORS_ALLOW_METHODS = config.get("CORS_ALLOW_METHODS", cast=Csv(), default="*")
+CORS_ALLOW_HEADERS = config.get("CORS_ALLOW_HEADERS", cast=Csv(), default="*")
