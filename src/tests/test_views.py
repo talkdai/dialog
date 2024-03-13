4aeaec060f3561261e2eaf96d03975a0992782b9
Vinicius Mesel <4984147+vmesel@users.noreply.github.com>
2024-03-13 16:50:13 -0300

    Adds pytest and testing env

--- src/tests/test_views.py
+++ src/tests/test_views.py
@@ -1,5 +1,4 @@
-import unittest
-import responses
+from dialog.models import ChatMessages
 
 def test_health(client):
     response = client.get("/health")
@@ -11,21 +10,26 @@ def test_create_session(client, mocker):
     assert response.status_code == 200
     assert "chat_id" in response.json()
 
-def test_post_message_no_session_id(client, mocker):
-    llm_mock = mocker.patch('main.post_message.get_llm_class')
-    llm_mock.return_value = mocker.Mock()
-    response = client.post("/chat/lala", json={"message": "Hello"})
+def test_post_message_no_session_id(client, chat_session, mocker):
+    llm_mock = mocker.patch('main.get_llm_class')
+    llm_mock.process.return_value = {"text": "Hello"}
+    session_id = chat_session["chat_id"]
+    response = client.post(f"/chat/{session_id}", json={"message": "Hello"})
     assert llm_mock.called
     assert response.status_code == 200
+    assert "message" in response.json()
 
 
-# def test_post_message_no_session_id(self):
-#     response = client.post("/ask", json={"message": "Hello"})
-#     self.assertEqual(response.status_code, 200)
-#     self.assertIn("message", response.json())
+def test_ask_question_no_session_id(client, chat_session, mocker):
+    llm_mock = mocker.patch('main.get_llm_class')
+    llm_mock.process.return_value = {"text": "Hello"}
+    response = client.post("/ask", json={"message": "Hello"})
+    assert response.status_code == 200
+    assert "message" in response.json()
 
-# def test_get_chat_content(self):
-#     response = client.get(f"/chat/{self.session_id}")
-#     self.assertEqual(response.status_code, 200)
-#     self.assertIn("message", response.json())
-
+def test_get_chat_content(client, chat_session, dbsession):
+    chat = ChatMessages(session_id=chat_session["chat_id"], message="Hello")
+    dbsession.add(chat)
+    response = client.get(f"/chat/{chat_session['chat_id']}")
+    assert response.status_code == 200
+    assert "message" in response.json()
