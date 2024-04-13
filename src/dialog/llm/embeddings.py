54184920bc8268c01417478989a556918872aa67
Vinicius Mesel <4984147+vmesel@users.noreply.github.com>
2024-04-13 15:58:57 -0300

    Removes certain embedding parts

--- src/dialog/llm/embeddings.py
+++ src/dialog/llm/embeddings.py
@@ -10,19 +10,3 @@ from dialog_lib.embeddings import generate
 
 
 EMBEDDINGS_LLM = OpenAIEmbeddings(openai_api_key=Settings().OPENAI_API_KEY)
-
-def generate_embeddings(documents: List[str]):
-    return generate.generate_embeddings(documents, EMBEDDINGS_LLM)
-
-
-def generate_embedding(document: str):
-    """
-    Generate embeddings for a single instance of document
-    """
-    return generate.generate_embedding(document, EMBEDDINGS_LLM)
-
-
-def get_most_relevant_contents_from_message(message, top=5, dataset=None, session=None):
-    return generate.get_most_relevant_contents_from_message(
-        message, top, dataset, session, EMBEDDINGS_LLM, cosine_similarity_threshold=Settings().COSINE_SIMILARITY_THRESHOLD
-    )
