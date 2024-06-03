4a143bc72c43971b4e1cec04f74ff5f6d21ad96a
Luan Fernandes <luan.fernandes@buser.com.br>
2024-06-02 23:21:57 -0300

    fix typo when bringing code to substitute

--- src/load_csv.py
+++ src/load_csv.py
@@ -1,88 +1,4 @@
 import argparse
-import hashlib
-
-import pandas as pd
-from sqlalchemy import text
-
-from dialog_lib.embeddings.generate import generate_embeddings
-from dialog.llm.embeddings import EMBEDDINGS_LLM
-from dialog_lib.db.models import CompanyContent
-from dialog.db import get_session
-
-session = next(get_session())
-
-def load_csv_and_generate_embeddings(path: str, cleardb=False, embed_columns: None | list[str] = None) -> None:
-    if not embed_columns:
-        embed_columns = ["content"]
-    df = pd.read_csv(path)
-    necessary_cols = ["category", "subcategory", "question", "content"]
-    for col in necessary_cols:
-        if col not in df.columns:
-            raise Exception(f"Column {col} not found in {path}")
-
-    if "dataset" in df.columns:
-        necessary_cols.append("dataset")
-
-    df = df[necessary_cols]
-
-    # Create primary key column using category, subcategory, and question
-    df["primary_key"] = df["category"] + df["subcategory"] + df["question"]
-    df["primary_key"] = df["primary_key"].apply(
-        lambda row: hashlib.md5(row.encode()).hexdigest()
-    )
-
-    if cleardb:
-        session.query(CompanyContent).delete()
-        session.commit()
-
-    df_in_db = pd.read_sql(
-        text(
-            f"SELECT category, subcategory, question, content, dataset FROM {CompanyContent.__tablename__}"
-        ),
-        session.get_bind(),
-    )
-
-    # Create primary key column using category, subcategory, and question for df_in_db
-    new_keys = set(df["primary_key"])
-    if not df_in_db.empty:
-        df_in_db["primary_key"] = df_in_db["category"] + df_in_db["subcategory"] + df_in_db["question"]
-        df_in_db["primary_key"] = df_in_db["primary_key"].apply(
-            lambda row: hashlib.md5(row.encode()).hexdigest()
-        )
-        new_keys = set(df["primary_key"]) - set(df_in_db["primary_key"])
-
-    # Filter df for keys present in df and not present in df_in_db
-    df_filtered = df[df["primary_key"].isin(new_keys)].copy()
-
-    print("Generating embeddings for new questions...")
-    print("New questions:", len(df_filtered))
-    if len(df_filtered) == 0:
-        return
-
-    print("embed_columns: ", embed_columns)
-    df_filtered.drop(columns=["primary_key"], inplace=True)
-    df_filtered["embedding"] = generate_embeddings(
-        list(df_filtered[embed_columns].agg('\n'.join, axis=1)),
-        embedding_llm_instance=EMBEDDINGS_LLM
-    )
-    df_filtered.to_sql(
-        CompanyContent.__tablename__,
-        session.get_bind(),
-        if_exists="append",
-        index=False,
-    )
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--path", type=str, required=False, default="./know.csv")
-    parser.add_argument("--cleardb", action="store_true")
-    parser.add_argument("--embed-columns", default="content")
-    args = parser.parse_args()
-
-    load_csv_and_generate_embeddings(
-        args.path, args.cleardb, args.embed_columns.split(','))
-import argparse
 from typing import List
 import hashlib
 import csv
@@ -115,6 +31,7 @@ def _get_csv_cols(path: str) -> List[str]:
         reader = csv.DictReader(f)
         return reader.fieldnames
 
+
 def retrieve_docs_from_vectordb() -> List[Document]:
     """Retrieve all documents from the vector store."""
     company_contents: List[CompanyContent] = session.query(CompanyContent).all()
@@ -129,6 +46,7 @@ def retrieve_docs_from_vectordb() -> List[Document]:
         )
         for content in company_contents
     ]
+
 
 def documents_to_company_content(doc: Document, embedding: float) -> CompanyContent:
     """Transform each langchain's Document and its embedding to a CompanyContent object."""
@@ -182,7 +100,9 @@ def load_csv_and_generate_embeddings(path, cleardb=False, embed_columns=("conten
 
     # Keep only new keys
     docs_filtered: List[Document] = [
-        doc for doc in docs if get_document_pk(doc, pk_metadata_fields) not in existing_pks
+        doc
+        for doc in docs
+        if get_document_pk(doc, pk_metadata_fields) not in existing_pks
     ]
     if len(docs_filtered) == 0:
         return
